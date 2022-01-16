bb04b5c4e6152f3ff268e338ce5b551f02c21c6e
priyan <45042030+priyan1995@users.noreply.github.com>
2022-01-16 12:27:14 +0530

    completed rendering test data

--- main.js
+++ main.js
@@ -37,6 +37,8 @@ const xhr = new XMLHttpRequest();
 
 xhr.onreadystatechange = function () {
     if (xhr.readyState == 4 && xhr.status == 200) {
+        // console.log(xhr.responseText);
+        // console.log(JSON.parse(xhr.responseText));
         displayData(JSON.parse(xhr.responseText));
     }
 }
